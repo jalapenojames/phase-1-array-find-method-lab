18bd782357cccb65b0052df846707b34be8e74ee
James <jaames.chen@gmail.com>
2021-04-08 14:09:06 -0400

    Completed array find method lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+]
+
+function superbowlWin(arrayOfObjects) {
+    let ind = arrayOfObjects.findIndex(obj => {
+        if(obj["result"] === "W")
+            return true
+        else
+            return undefined
+    })
+    if(ind >=0 )
+        return arrayOfObjects[ind]["year"]
+    else
+        return undefined
+}
 
